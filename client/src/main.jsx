1159c83614474c5ab0761385455e3cdbc69d8c89
Lalitmehra9720 <139990443+Lalitmehra9720@users.noreply.github.com>
2026-03-31 20:04:30 +0530

    fix: scrollToTop behaviour on about page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import {Toaster} from "react-hot-toast";
+import ScrollToTop from "./utils/ScrollToTop";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <App />
         <Toaster position="top-right"/>
